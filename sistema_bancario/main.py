79acfcceb7173b312452e5a72d437c6c4650cd60
Guilherme Campos <guilhermev272@gmail.com>
2024-10-29 14:23:39 -0300

    Update main.py

--- sistema_bancario/main.py
+++ sistema_bancario/main.py
@@ -1,5 +1,7 @@
 from cliente import salvar_dados
 
+
+indice = 0
 while True:
 	print('Bem vindo, você já é clinte?')
 	acesso = input('[SIM] / [NÃO]:\n ').upper()
@@ -13,23 +15,53 @@ while True:
 		break
 
 	elif acesso == 'NÃO':
+		indice += 1
 		while True:
-			print('Informe-nos seus dados de CPF, Nome, Idade e Celular para criarmos sua conta:')
+			print('Informe-nos seus dados: Nome, Sobrenome, CPF, Idade e Celular para criarmos sua conta:')
 			_nome = input('Nome: ')
+			if _nome.isalpha():
+				...
+			else:
+				print('Por favor, digite apenas letras.')
+				print()
+				continue
+
+			_sobrenome = input('Sobrenome: ')
+			if _sobrenome.isalpha():
+				...
+			else:
+				print('Por favor, digite apenas letras.')
+				print()
+				continue
 
 			cpf = input('CPF: ')
+			if cpf.isdigit():
+				...
+			else:
+				print('Por favor, digite apenas números.')
+				print()
+				continue
 			tamanho_cpf = len(cpf)
 			if tamanho_cpf < 11 or tamanho_cpf > 11:
 				print('Por favor, digite CPF de forma correta.')
 				print()
 				continue
 			
-			try:
-				idade = input('Idade: ')
-			except:
-				print('Digite apenas números.')
+			idade = input('Idade: ')
+			if idade.isdigit():
+				...
+			else:
+				print('Por favor, digite apenas números.')
+				print()
+				continue
 
 			celular = input('Celular: ')
+			if celular.isdigit():
+				...
+			else:
+				print('Por favor, digite apenas números.')
+				print()
+				continue
 			tamanho_celular = len(celular)
 			if tamanho_celular < 11 or tamanho_celular > 11:
 				print('Por favor, digite o número de celular de forma correta.')
@@ -46,15 +78,17 @@ while True:
 				print()
 				print('Senha muito grande.')
 				continue
+			print()
 
 			_cpf = int(cpf)
 			_idade = int(idade)
 			_celular = int(celular)
 
-			salvar_dados(0, _nome, _cpf, _idade, _celular, _senha)
-			print('Seus dados foram coletados')
-			
+			salvar_dados(indice, _nome, _sobrenome, _cpf, _idade, _celular, _senha)
+			print('Seus dados foram coletados, agora efetue o login.')
+			print()
 			break
+		
 
 	else:
 		print('Por favor esolha uma das opções.')
