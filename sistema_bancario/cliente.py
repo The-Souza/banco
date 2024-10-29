ecba1ea07a84c8c4775d148c6a9aae0fa5009942
Guilherme Campos <guilhermev272@gmail.com>
2024-10-29 13:09:49 -0300

    Update cliente.py

--- sistema_bancario/cliente.py
+++ sistema_bancario/cliente.py
@@ -2,58 +2,64 @@ import json
 from pathlib import Path
 import shutil
 import tempfile
+import pprint
+import secrets
 
-class Cliente:
-    def __init__(self, nome: str, cpf: int, idade: int, celular: int) -> None:
-        self.nome = nome
-        self.cpf = cpf
-        self.idade = idade
-        self.celular = celular
+random = secrets.SystemRandom()
 
-    @property
-    def nome(self):
-        return self._nome
+# class Cliente:
+#     def __init__(self, nome: str, cpf: int, idade: int, celular: int) -> None:
+#         self.nome = nome
+#         self.cpf = cpf
+#         self.idade = idade
+#         self.celular = celular
 
-    @nome.setter
-    def nome(self, nome: str):
-        self._nome = nome
+#     @property
+#     def nome(self):
+#         return self._nome
 
-    @property
-    def cpf(self):
-        return self._cpf
+#     @nome.setter
+#     def nome(self, nome: str):
+#         self._nome = nome
 
-    @cpf.setter
-    def cpf(self, cpf: int):
-        self._cpf = cpf
+#     @property
+#     def cpf(self):
+#         return self._cpf
+
+#     @cpf.setter
+#     def cpf(self, cpf: int):
+#         self._cpf = cpf
         
-    @property
-    def idade(self):
-        return self._idade
+#     @property
+#     def idade(self):
+#         return self._idade
 
-    @idade.setter
-    def idade(self, idade: int):
-        self._idade = idade
+#     @idade.setter
+#     def idade(self, idade: int):
+#         self._idade = idade
 
-    @property
-    def celular(self):
-        return self._celular
+#     @property
+#     def celular(self):
+#         return self._celular
 
-    @celular.setter
-    def celular(self, celular: int):
-        self._celular = celular
+#     @celular.setter
+#     def celular(self, celular: int):
+#         self._celular = celular
 
-    def __repr__(self):
-        class_name = type(self).__name__
-        attrs = f'({self.cpf!r}, {self.nome!r}, {self.idade!r}, {self.celular!r})'
-        return f'{class_name} -> {attrs}'
+#     def __repr__(self):
+#         class_name = type(self).__name__
+#         attrs = f'({self.cpf!r}, {self.nome!r}, {self.idade!r}, {self.celular!r})'
+#         return f'{class_name} -> {attrs}'
 
 
 CAMINHO_ARQUIVO = Path(__file__).parent / 'banco_de_dados.json'
 
-with open(CAMINHO_ARQUIVO, 'r', encoding='utf8') as arquivo:
-    dados = json.load(arquivo)
+def ver_dados(indice):
+    with open(CAMINHO_ARQUIVO, 'r', encoding='utf8') as arquivo:
+        dados = json.load(arquivo)
+    print(dados[indice])
 
-def salvar_dados(indice: int, nome: str, cpf: int, idade: int, celular: int, agencia: str, conta: int, senha: str):
+def salvar_dados(indice: int, nome: str, cpf: int, idade: int, celular: int, senha: str):
     with open(CAMINHO_ARQUIVO, 'r', encoding='utf-8') as arquivo, \
         tempfile.NamedTemporaryFile('w', delete=False) as saida:
         dados = json.load(arquivo)
@@ -61,12 +67,11 @@ def salvar_dados(indice: int, nome: str, cpf: int, idade: int, celular: int, age
         dados[indice]['_cpf'] = cpf
         dados[indice]['_idade'] = idade
         dados[indice]['_celular'] = celular
-        dados[indice]['_agencia'] = agencia
-        dados[indice]['_conta'] = conta
         dados[indice]['_senha'] = senha
         json.dump(dados, saida, ensure_ascii=False, indent=4, separators=(',',':'))
-
+        
     shutil.move(saida.name, CAMINHO_ARQUIVO)
-    print(dados[indice])
-
-print(dados[::1])
+        
+if __name__ == '__main__':
+    salvar_dados(0, 'gg', 123, 22, 1232, 'lincecaloteira')
+    # ver_dados(0)
